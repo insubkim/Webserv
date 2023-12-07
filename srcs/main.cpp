5140fc5635d587edff771a6f1e1491ca033f7eee
inskim <j.j.kant1948@gmail.com>
2023-12-07 16:57:13 +0900

    refactor: refactor main.cpp

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,15 +1,13 @@
 #include "Server.hpp"
 
 int main(int argc, char** argv) {
-  const char *config_file = (argc == 2 ? argv[1] : "webserv.config");
-  try {
-    Server  server(config_file);
-    if (argc > 2) {
-      std::cout << "usage: ./webserv [config_file]" << std::endl;
-      return 1;
-    }
-    server.init();
-    server.run();
-  } catch (std::exception& e) { std::cout << e.what() << std::endl; }
+  if (argc > 2) {
+    std::cout << "usage: ./webserv [config_file]" << std::endl;
+    return 1;
+  }
+
+  Server  server(argc == 2 ? argv[1] : "./utils/webserv.config");
+  server.init();
+  server.run();
   return 0;
 }
